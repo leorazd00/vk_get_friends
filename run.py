85bc3af0b30b71b267fbfc659430193ec3b4911e
Leonid <razdobreev.leo@gmail.com>
2022-06-10 20:43:28 +0700

    add type for argspace argument

--- run.py
+++ run.py
@@ -5,14 +5,15 @@ from parser_friends import ParserFriends
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('token', help='Your authorized token')
-parser.add_argument('user_id', help='User ID for which the report is being generated')
-parser.add_argument('file_extension', help='Output file format', nargs='?', default='csv')
-parser.add_argument('path_to_save', help='Path to the output file', nargs='?', default=f'{os.getcwd()}/report')
+parser.add_argument('--token', help='Your authorized token', type=str)
+parser.add_argument('--user_id', help='User ID for which the report is being generated', type=int)
+parser.add_argument('--file_extension', help='Output file format', nargs='?', type=str, default='csv')
+parser.add_argument('--path_to_save', help='Path to the output file', nargs='?', 
+                    type=str, default=f'{os.getcwd()}/report')
 
 args = parser.parse_args() 
 
 
 if __name__ == '__main__':
     parse_data = ParserFriends(args.token)
-    parse_data(args.user_id, args.path_to_save)
+    parse_data(args.user_id, args.path_to_save, extension_file=args.file_extension)
