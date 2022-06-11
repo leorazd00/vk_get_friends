0bf59a23b377ffed5871dd901d92be13552f52fb
Leonid <razdobreev.leo@gmail.com>
2022-06-11 15:41:22 +0700

    formalization of the PEP8

--- run.py
+++ run.py
@@ -2,19 +2,22 @@ import os
 import argparse
 from parser_friends import ParserFriends
 
+default_path = f'{os.getcwd()}/report'
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--token', help='Your authorized token', type=str)
-parser.add_argument('--user_id', help='User ID for which the report is being generated', type=int)
-parser.add_argument('--file_extension', help='Output file format', nargs='?', type=str, default='csv')
-parser.add_argument('--path_to_save', help='Path to the output file', nargs='?',
-                    type=str, default=f'{os.getcwd()}/report')
+parser.add_argument('--user_id', help='User ID for which the report is being \
+                                       generated', type=int)
+parser.add_argument('--file_extension', help='Output file format', nargs='?',
+                    type=str, default='csv')
+parser.add_argument('--path_to_save', help='Path to the output file',
+                    nargs='?',  type=str, default=default_path)
 
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
     print('Connecting to vk_api...')
-    parse_data = ParserFriends(args.token, args.file_extension)
-    parse_data(args.user_id, args.path_to_save)
+    parse_data = ParserFriends(args.token, args.user_id, args.file_extension)
+    parse_data(args.path_to_save)
